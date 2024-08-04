4f1c274e6c70164eb6042f3e681588d40ae1194b
Gitesh <gitesh.sarvaiya28@gmail.com>
2024-08-04 18:48:03 +0530

    auth setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,30 @@
+require('dotenv').config();
 const express = require('express');
+const { auth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
+
+
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.AUTH0_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+};
+
+app.use(auth(config));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  // res.send('Hello World!');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });
 
 app.listen(port, () => {
