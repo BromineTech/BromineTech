540090bee0a50719f3ba17c971365aa060dd680c
C14rc <rajeevdewangan10@gmail.com>
2024-08-08 22:27:24 +0530

    dns initialised

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
 
 router.get('/', (req, res) => {
-  if (req.oidc.isAuthenticated()) {
-    res.redirect('/project/all');
-  }
+
+  res.send("Hello")
+  // if (req.oidc.isAuthenticated()) {
+  //   res.redirect('/project/all');
+  // }
 });
 
 module.exports = router;
