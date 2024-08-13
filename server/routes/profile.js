abc56b85c0a0ba4276dd026b23666b244df6211b
Gitesh <gitesh.sarvaiya28@gmail.com>
2024-08-13 18:07:16 +0530

    invite email modified

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
 
 router.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user.nickname));
+  res.send(JSON.stringify(req.oidc.user));
 });
 
 module.exports = router;
