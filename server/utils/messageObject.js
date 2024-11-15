07c1c34ea1080e0ddbb13c3013daca25c6d04670
Gitesh <gitesh.sarvaiya28@gmail.com>
2024-11-15 09:55:33 +0530

    isActivity ka isEvent ka structure define

--- server/utils/messageObject.js
+++ server/utils/messageObject.js
@@ -8,9 +8,9 @@ const messageObject = {
     isText: null, //yaha pe humlog bas text le rahe hai jaise praject name, description, (true ya false)
     text: null,   //milestone name ya uska description
     isActivity: null, //yaha pe jo database mein hai wo saari cheezein yaha pe hongi (true ya false)
-    Activity: null,
+    activity: null,
     isAction: null, //yaha pe humlog wo saari cheezein update karenge jo nahi text hai nahi comment hai (true ya false)
-    Action: null //   jaise link, issue banana ya assign karna , milestone banana aur milestone attach karna
+    action: null //   jaise link, issue banana ya assign karna , milestone banana aur milestone attach karna
                   //  target ya assign date change karna
   };
   
